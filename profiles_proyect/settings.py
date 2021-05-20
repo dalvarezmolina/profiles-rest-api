9ac803f1b0b2c32e18bd3c5b1ea4a6a3bf51427c
dalvarzmolina <dalvarez@zuntexapparel.com>
2021-05-20 11:02:39 -0600

    configured for deployment

--- profiles_proyect/settings.py
+++ profiles_proyect/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$_x#-guwjtz#ig0v2@m!&#ljxq#t=t$vbp&e*vbhx3!886r58('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -122,3 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
