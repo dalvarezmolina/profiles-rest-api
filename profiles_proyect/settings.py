e30de534569a2b79cbdfec974ffbae4a3e60f6d1
dalvarzmolina <dalvarez@zuntexapparel.com>
2021-05-20 11:21:14 -0600

    added alowd hosts

--- profiles_proyect/settings.py
+++ profiles_proyect/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '$_x#-guwjtz#ig0v2@m!&#ljxq#t=t$vbp&e*vbhx3!886r58('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-18-224-6-47.us-east-2.compute.amazonaws.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
